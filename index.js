866bb31d21ccc38f109102fc21d08a13529bf0ba
Stephen Kang <kang.stephen94@gmail.com>
2018-07-29 18:43:46 -0700

    Add route

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-// const { send } = require("micro");
+const { send } = require("micro");
 var pg = require("pg");
-var fs = require("fs");
 var conString = process.env.DATABASE_URL || "postgres://localhost:5432/swish";
 var client = new pg.Client(conString);
+// var fs = require("fs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 client.connect();
 
-// Code used to seed database with data.json file
+module.exports = (req, res) => {
+  const query = {
+    text: 'SELECT * FROM players LIMIT 10'
+  };
+  client.query(query, (err, response) => {
 
-fs.readFile('data.json', 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  const parsedData = JSON.parse(data)
-
-  if (parsedData) {
-    for (var i = 0; i < data.length; i++) {
-    
-    const text = 'INSERT INTO players(id, player) VALUES($1, $2)'
-    const value = [i, JSON.stringify(parsedData[i])]
-
-    client.query(text, value, (err, res) => {
-      if (err) {
-        console.log(err.stack)
-      } else {
-        // console.log(res.rows[0])
-      }
-    })
-  }
+    send(res, 200, response.rows);
+  });
 }
 
+// Code used to seed database with data.json file
 
-});
+// fs.readFile('data.json', 'utf8', function (err, data) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   const parsedData = JSON.parse(data)
+
+//   if (parsedData) {
+//     for (var i = 0; i < data.length; i++) {
+    
+//     const text = 'INSERT INTO players(id, player) VALUES($1, $2)'
+//     const value = [i, JSON.stringify(parsedData[i])]
+
+//     client.query(text, value, (err, res) => {
+//       if (err) {
+//         console.log(err.stack)
+//       } else {
+//         // console.log(res.rows[0])
+//       }
+//     })
+//   }
+// }
+
+
+// });
+
